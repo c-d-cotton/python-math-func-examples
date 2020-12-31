55bf7523d94860fe12ee4d4bc0f265a5238061ed
Christopher Cotton <cdc2175@columbia.edu>
2020-12-31 07:01:33 -0500

    Adjust preambles/README/submodule setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction
+Test functions for https://github.com/c-d-cotton/python-math-func
+
+# Setup
+Run setup_submodules.sh to add in required submodules.
--- bayes/bayesian_test_func.py
+++ bayes/bayesian_test_func.py
@@ -1,55 +1,9 @@
 #!/usr/bin/env python3
-# PYTHON_PREAMBLE_START_STANDARD:{{{
-
-# Christopher David Cotton (c)
-# http://www.cdcotton.com
-
-# modules needed for preamble
-import importlib
 import os
 from pathlib import Path
 import sys
 
-# Get full real filename
-__fullrealfile__ = os.path.abspath(__file__)
-
-# Function to get git directory containing this file
-def getprojectdir(filename):
-    curlevel = filename
-    while curlevel is not '/':
-        curlevel = os.path.dirname(curlevel)
-        if os.path.exists(curlevel + '/.git/'):
-            return(curlevel + '/')
-    return(None)
-
-# Directory of project
-__projectdir__ = Path(getprojectdir(__fullrealfile__))
-
-# Function to call functions from files by their absolute path.
-# Imports modules if they've not already been imported
-# First argument is filename, second is function name, third is dictionary containing loaded modules.
-modulesdict = {}
-def importattr(modulefilename, func, modulesdict = modulesdict):
-    # get modulefilename as string to prevent problems in <= python3.5 with pathlib -> os
-    modulefilename = str(modulefilename)
-    # if function in this file
-    if modulefilename == __fullrealfile__:
-        return(eval(func))
-    else:
-        # add file to moduledict if not there already
-        if modulefilename not in modulesdict:
-            # check filename exists
-            if not os.path.isfile(modulefilename):
-                raise Exception('Module not exists: ' + modulefilename + '. Function: ' + func + '. Filename called from: ' + __fullrealfile__ + '.')
-            # add directory to path
-            sys.path.append(os.path.dirname(modulefilename))
-            # actually add module to moduledict
-            modulesdict[modulefilename] = importlib.import_module(''.join(os.path.basename(modulefilename).split('.')[: -1]))
-
-        # get the actual function from the file and return it
-        return(getattr(modulesdict[modulefilename], func))
-
-# PYTHON_PREAMBLE_END:}}}
+__projectdir__ = Path(os.path.dirname(os.path.realpath(__file__)) + '/../')
 
 import functools
 import numpy as np
@@ -75,7 +29,9 @@ def mh_basic():
         ll = np.sum(np.log(density_separate))
         return(ll)
 
-    results = importattr(__projectdir__ / Path('submodules/python-math-func/bayesian_func.py'), 'metropolis_hastings')(loglikelihoodfunc, [0.1], [0], 10000, printdetails = False, logposterior = True)
+    sys.path.append(str(__projectdir__ / Path('submodules/python-math-func/')))
+    from bayesian_func import metropolis_hastings
+    results = metropolis_hastings(loglikelihoodfunc, [0.1], [0], 10000, printdetails = False, logposterior = True)
     print(np.mean(np.array(results)[1000: , :], axis = 0))
 
 
@@ -99,10 +55,14 @@ def mh_save():
         return(ll)
 
     # save results
-    importattr(__projectdir__ / Path('submodules/python-math-func/bayesian_func.py'), 'metropolis_hastings')(loglikelihoodfunc, [0.1], [0], 10000, printdetails = False, logposterior = True, savefile = __projectdir__ / Path('bayes/temp/mh_save/results.csv'))
+    sys.path.append(str(__projectdir__ / Path('submodules/python-math-func/')))
+    from bayesian_func import metropolis_hastings
+    metropolis_hastings(loglikelihoodfunc, [0.1], [0], 10000, printdetails = False, logposterior = True, savefile = __projectdir__ / Path('bayes/temp/mh_save/results.csv'))
 
     # load results
-    results = importattr(__projectdir__ / Path('submodules/python-math-func/bayesian_func.py'), 'distfromfile')(__projectdir__ / Path('bayes/temp/mh_save/results.csv'), burnindelete = 1000)
+    sys.path.append(str(__projectdir__ / Path('submodules/python-math-func/')))
+    from bayesian_func import distfromfile
+    results = distfromfile(__projectdir__ / Path('bayes/temp/mh_save/results.csv'), burnindelete = 1000)
     print(np.mean(results, axis = 0))
     
 
@@ -133,10 +93,14 @@ def mh_pool():
     loglikelihoodfunc = functools.partial(mh_pool_aux_loglikelihoodfunc, mean, draws)
 
     # save results
-    importattr(__projectdir__ / Path('submodules/python-math-func/bayesian_func.py'), 'metropolis_hastings_pool')(__projectdir__ / Path('bayes/temp/mh_pool/'), loglikelihoodfunc, [0.1], [0.1], 1000, printdetails = False, logposterior = True)
+    sys.path.append(str(__projectdir__ / Path('submodules/python-math-func/')))
+    from bayesian_func import metropolis_hastings_pool
+    metropolis_hastings_pool(__projectdir__ / Path('bayes/temp/mh_pool/'), loglikelihoodfunc, [0.1], [0.1], 1000, printdetails = False, logposterior = True)
 
     # load results
-    results = importattr(__projectdir__ / Path('submodules/python-math-func/bayesian_func.py'), 'distfromfolder')(__projectdir__ / Path('bayes/temp/mh_pool/'), burnindelete = 100, concatenate = True)
+    sys.path.append(str(__projectdir__ / Path('submodules/python-math-func/')))
+    from bayesian_func import distfromfolder
+    results = distfromfolder(__projectdir__ / Path('bayes/temp/mh_pool/'), burnindelete = 100, concatenate = True)
     print(np.mean(results, axis = 0))
 
 
@@ -162,7 +126,9 @@ def mh_bounds():
         ll = np.sum(np.log(density_separate))
         return(ll)
 
-    results = importattr(__projectdir__ / Path('submodules/python-math-func/bayesian_func.py'), 'metropolis_hastings')(loglikelihoodfunc, [0.1, 0.1], [0, 0.1], 10000, lowerboundlist = [None, 0], printdetails = False, logposterior = True)
+    sys.path.append(str(__projectdir__ / Path('submodules/python-math-func/')))
+    from bayesian_func import metropolis_hastings
+    results = metropolis_hastings(loglikelihoodfunc, [0.1, 0.1], [0, 0.1], 10000, lowerboundlist = [None, 0], printdetails = False, logposterior = True)
     print(np.mean(np.array(results)[1000: , :], axis = 0))
 
 
@@ -190,11 +156,17 @@ def mh_priors():
     # get priors
     priorlist_meansd = [['gamma', 0.15, 0.05], ['invgamma', 0.3, 0.05]]
     # get a priorlist based upon parameters rather than means and standard deviations
-    priorlist_parameters = importattr(__projectdir__ / Path('submodules/python-math-func/bayesian_func.py'), 'getpriorlist_convert')(priorlist_meansd)
+    sys.path.append(str(__projectdir__ / Path('submodules/python-math-func/')))
+    from bayesian_func import getpriorlist_convert
+    priorlist_parameters = getpriorlist_convert(priorlist_meansd)
     # get details on priors
-    prior_means, prior_sds, prior_lbs, prior_ubs = importattr(__projectdir__ / Path('submodules/python-math-func/bayesian_func.py'), 'getpriorlistdetails_parameters')(priorlist_parameters)
+    sys.path.append(str(__projectdir__ / Path('submodules/python-math-func/')))
+    from bayesian_func import getpriorlistdetails_parameters
+    prior_means, prior_sds, prior_lbs, prior_ubs = getpriorlistdetails_parameters(priorlist_parameters)
     # get a density function for the priors - this is NOT log
-    priorfunc = functools.partial(importattr(__projectdir__ / Path('submodules/python-math-func/bayesian_func.py'), 'getpriordensityfunc_aux'), priorlist_parameters)
+    sys.path.append(str(__projectdir__ / Path('submodules/python-math-func/')))
+    from bayesian_func import getpriordensityfunc_aux
+    priorfunc = functools.partial(getpriordensityfunc_aux, priorlist_parameters)
     # get scalelist based upon sds
     scalelist = [0.5 * sd for sd in prior_sds]
 
@@ -205,6 +177,8 @@ def mh_priors():
         """
         return(np.log(priorfunc(values)) + loglikelihoodfunc(values))
 
-    results = importattr(__projectdir__ / Path('submodules/python-math-func/bayesian_func.py'), 'metropolis_hastings')(posteriorfunc, scalelist, prior_means, 10000, lowerboundlist = prior_lbs, upperboundlist = prior_ubs, printdetails = False, logposterior = True)
+    sys.path.append(str(__projectdir__ / Path('submodules/python-math-func/')))
+    from bayesian_func import metropolis_hastings
+    results = metropolis_hastings(posteriorfunc, scalelist, prior_means, 10000, lowerboundlist = prior_lbs, upperboundlist = prior_ubs, printdetails = False, logposterior = True)
     print(np.mean(np.array(results)[1000: , :], axis = 0))
 
